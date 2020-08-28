2ef9d7a89bc620ca508d8a403093392c9100f40d
DeekshitaA <deekshita.amaravadi@gmail.com>
2020-08-28 17:43:15 -0400

    feat(db): Added Prisma and Postrgres DB connection to post to news table and retrieve news from db

---
+++ pages/api/news/create.tsx
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient({ log: ["query"] });
+
+    try {
+        const { news: newsData } = req.body;
+        const news = await prisma.news.create({
+            data: {
+                siteUrl: newsData.siteUrl,
+                imageUrl: newsData.imageUrl,
+                descr: newsData.descr,
+            },
+        });
+        res.status(201);
+        res.json({ news });
+    } catch (e) {
+        res.status(500);
+        res.json({ error: "Sorry unable to save news to database" });
+    } finally {
+        await prisma.disconnect()
+    }
+}
